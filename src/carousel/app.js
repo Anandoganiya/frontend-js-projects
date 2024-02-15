421855b9b255a4920e5853cb3b34aea2d96ce500
Anand Oganiya <anandoganiya@gmail.com>
2024-02-15 23:25:15 +0530

    updated carousel

--- src/carousel/app.js
+++ src/carousel/app.js
@@ -1,14 +1,31 @@
-const leftArrow = document.querySelector('.left-arrow')
-const rightArrow = document.querySelector('.right-arrow')
-const imgHolder = document.querySelector('.img-holder')
+const rightArrow = document.querySelector('.right-arrow');
+const leftArrow = document.querySelector('.left-arrow');
+const imageContainerElement = document.querySelector('.images-container');
 
-let selected = 0;
-let rightCount = 1;
-rightArrow.addEventListener('click', (e) => {
-    selected += 300;
-    imgHolder.style.transform = `translateX(-${selected}px)`;
+let  addOns = 0;
+rightArrow.addEventListener('click', (event) => {
+    addOns += 250;
+    imageContainerElement.style.transform = `translateX(-${addOns}px)`;
+    console.log(addOns)
+    if (addOns <= 1000) {
+        rightArrow.disabled = false;
+    } else {
+        rightArrow.disabled = true;
+        leftArrow.disabled = true;
+        
+    }
 })
-leftArrow.addEventListener('click', (e) => {
-    selected -= 300;
-    imgHolder.style.transform = `translateX(-${selected}px)`
+
+leftArrow.addEventListener('click', (event) => {
+    addOns -= 250;
+    imageContainerElement.style.transform = `translateX(-${addOns}px)`;
+    if (addOns > 0) {
+        leftArrow.disabled  = false;
+    }
+    else {
+        leftArrow.disabled = true;
+        rightArrow.disabled = false;
+
+    }
+    console.log(addOns)
 })
