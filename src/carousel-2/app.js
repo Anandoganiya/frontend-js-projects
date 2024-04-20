0c5d1b6b7f0836b900cdf06b00f012dd895d3f1f
Anand Oganiya <anandoganiya@gmail.com>
2024-04-21 01:05:22 +0530

    added

--- src/carousel-2/app.js
+++ src/carousel-2/app.js
@@ -4,11 +4,25 @@ const imageContainer = document.querySelector('.image-container')
 const infiniteScroll = document.querySelector('#infinity-scroll')
 const autoPlay = document.querySelector('#auto-play')
 const autoPlayInterval = document.querySelector('#auto-play-interval')
+const dots = document.querySelector('.dot-wrapper');
 let currentSlide = 0;
 let slidesLength = 0;
 let intervalId = null;
 
+dots.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('dot')) return;
+    const index = parseInt(e.target.dataset.index);
+    currentSlide = index;
+    handleSliding();
+    setButtonState();
+    const prevDot = dots.querySelector('.dot-active');
+    prevDot.classList.remove('dot-active');
+    e.target.classList.add('dot-active');
+})
 
+infiniteScroll.addEventListener('click', (e) => {
+    setButtonState();
+})
 autoPlay.addEventListener('click', (e) => {
     autoPlayInterval.disabled = autoPlay.checked;
     if (e.target.checked) {
@@ -18,7 +32,13 @@ autoPlay.addEventListener('click', (e) => {
     } else {
         clearInterval(intervalId);
     }
+   
 })
+
+function setButtonState() {
+    moveLeftButton.disabled = (currentSlide == 0 && !infiniteScroll.checked) ? true : false;
+    moveRightButton.disabled = (currentSlide == slidesLength -1 && !infiniteScroll.checked) ? true : false;
+}
 
 function moveToRight() {
     if (currentSlide == slidesLength - 1) {
@@ -27,6 +47,11 @@ function moveToRight() {
     }else
     ++currentSlide;
     handleSliding();
+    const prevDot = dots.querySelector('.dot-active');
+    prevDot.classList.remove('dot-active');
+    const currentDot = dots.querySelector(`[data-index='${currentSlide}']`);
+    currentDot.classList.add('dot-active')
+    setButtonState();
 }
 
 moveRightButton.addEventListener('click', e => {
@@ -40,6 +65,11 @@ moveLeftButton.addEventListener('click', e => {
     }else
     --currentSlide;
     handleSliding();
+    setButtonState()
+    const prevDot = dots.querySelector('.dot-active');
+    prevDot.classList.remove('dot-active');
+    const currentDot = dots.querySelector(`[data-index='${currentSlide}']`);
+    currentDot.classList.add('dot-active')
 })
 
 function init() {
@@ -48,8 +78,20 @@ function init() {
     imageItems.forEach((item, index) => {
         item.style.transform = `translateX(${100 * index}%)`;
         imageContainer.appendChild(item);
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.classList.add('dot');
+        button.role = 'tab';
+        button.dataset.index = index;
+        li.appendChild(button);
+        dots.appendChild(li);
+    
+        if (index === 0) {
+          button.classList.add('dot-active');
+        }
     })
     slidesLength = imageItems.length;
+    setButtonState()
 }
 
 function handleSliding() {
